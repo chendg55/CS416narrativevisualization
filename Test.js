100d7849b000652ce8f0b9fd5cacf765d1ef744e
chendg55 <110141958+chendg55@users.noreply.github.com>
2022-07-27 22:51:40 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,6 +1,8 @@
 var data = [4,8,15,16,23,42];
-x = d3.scaleBand().domain([0,1,2,3,4,5]).range([0, 200])
-y = d3.scaleLinear().domain([0,42]).range([200, 0])
+width = 1400;
+height = 800;
+x = d3.scaleBand().domain([0,1,2,3,4,5]).range([0, width]);
+y = d3.scaleLinear().domain([0,42]).range([height, 0]);
 
 d3.select("svg").append("g")
  .attr("transform","translate(50,50)")
@@ -8,15 +10,15 @@ d3.select("svg").append("g")
  .data(data)
  .enter()
  .append("rect")
-  .attr("x",function(d,i) {return 100*i/3;})
-  .attr("y",function(d,i) {return (200-d/42*200);})
-  .attr("width",(100/3))
-  .attr("height",function(d,i) {return (d/42*200);});
+  .attr("x",function(d,i) {return width*i/6;})
+  .attr("y",function(d,i) {return (height-d/42*height);})
+  .attr("width",(width/6))
+  .attr("height",function(d,i) {return (d/42*height);});
 
 d3.select("svg").append("g")
  .attr("transform","translate(50,50)")
  .call(d3.axisLeft(y));
 
 d3.select("svg").append("g")
- .attr("transform","translate(50,250)")
+ .attr("transform","translate(50, height+50)")
  .call(d3.axisBottom(x));
