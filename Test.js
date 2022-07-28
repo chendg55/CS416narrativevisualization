1281614fcba3da214471d94efa6bd5bb6b43b218
chendg55 <110141958+chendg55@users.noreply.github.com>
2022-07-28 01:05:19 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,24 +1,30 @@
-var data = [4,8,15,16,23,42];
-width = 1400;
-height = 800;
-x = d3.scaleBand().domain([0,1,2,3,4,5]).range([0, width]);
-y = d3.scaleLinear().domain([0,42]).range([height, 0]);
+async function init() {
+// Import data
+var data = await d3.csv("COVID 2021 USA.csv");
+ 
+// Canvas size, margins, chart size
+svg_width = 1500;
+svg_height = 900;
+const margin = { top: 30, bottom: 30, left: 30, right: 30 };
+chart_width = svg_width - margin.left - margin.right;
+chart_height = svg_height - margin.top - margin.bottom;
+ 
+// Create scales
+var x = d3.scaleTime()
+          .domain(d3.extent(data.Date, function(d) { return new Date(d.date); }))
+          .range([0, chart_width]);
+var y = d3.scaleLinear().domain([0, d3.max(data.Cases)]).range([chart_height, 0]);
 
-d3.select("svg").append("g")
- .attr("transform","translate(50,50)")
- .selectAll("rect")
- .data(data)
- .enter()
- .append("rect")
-  .attr("x",function(d,i) {return width*i/6;})
-  .attr("y",function(d,i) {return (height-d/42*height);})
-  .attr("width",(width/6))
-  .attr("height",function(d,i) {return (d/42*height);});
+// Line
 
+
+// Add Y axis
 d3.select("svg").append("g")
- .attr("transform","translate(50,50)")
+ .attr("transform",`translate(${margin.left},${margin.top})`)
  .call(d3.axisLeft(y));
 
+// Add X axis
 d3.select("svg").append("g")
- .attr("transform",translate(50, height+50))
+ .attr("transform",`translate(${margin.left},${margin.top + chart_height})`)
  .call(d3.axisBottom(x));
+}
