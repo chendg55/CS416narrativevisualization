18a6115a5b9656e2fe614ac505156243dac4861b
chendg55 <110141958+chendg55@users.noreply.github.com>
2022-07-27 21:55:46 -0400

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,21 @@
+x = d3.scaleBand().domain([0,1,2,3,4,5]).range([0, 200])
+y = d3.scaleLinear().domain([0,42]).range([200, 0])
+
+d3.select("svg").append("g")
+ .attr("transform","translate(50,50)")
+ .selectAll("rect")
+ .data(data)
+ .enter()
+ .append("rect")
+  .attr("x",function(d,i) {return 100*i/3;})
+  .attr("y",function(d,i) {return (200-d/42*200);})
+  .attr("width",(100/3))
+  .attr("height",function(d,i) {return (d/42*200);});
+
+d3.select("svg").append("g")
+ .attr("transform","translate(50,50)")
+ .call(d3.axisLeft(y));
+
+d3.select("svg").append("g")
+ .attr("transform","translate(50,250)")
+ .call(d3.axisBottom(x));
