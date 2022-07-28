061ffdbd6ee5640ecf4415153d5d4e5782d013d5
chendg55 <110141958+chendg55@users.noreply.github.com>
2022-07-28 02:51:26 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,30 +1,42 @@
-async function init() {
-// Import data
-var data = await d3.csv("COVID 2021 USA.csv");
+//Import data
+var data = d3.csv("https://raw.githubusercontent.com/chendg55/CS416narrativevisualization/gh-pages/COVID%202021%20USA.csv", d3.autoType);
+console.log(data)
  
 // Canvas size, margins, chart size
 svg_width = 1500;
 svg_height = 900;
-const margin = { top: 30, bottom: 30, left: 30, right: 30 };
+const margin = { top: 30, bottom: 30, left: 60, right: 30 };
 chart_width = svg_width - margin.left - margin.right;
 chart_height = svg_height - margin.top - margin.bottom;
+
  
 // Create scales
-var x = d3.scaleTime()
-          .domain(d3.extent(data.Date, function(d) { return new Date(d.date); }))
-          .range([0, chart_width]);
-var y = d3.scaleLinear().domain([0, d3.max(data.Cases)]).range([chart_height, 0]);
+const x = d3.scaleTime().domain([new Date("2021-01-01"), new Date("2021-12-31")]).range([0, chart_width])
+const y = d3.scaleLinear().domain([0, 34359601]).range([chart_height, 0])
 
 // Line
-
-
-// Add Y axis
-d3.select("svg").append("g")
- .attr("transform",`translate(${margin.left},${margin.top})`)
- .call(d3.axisLeft(y));
+const line = d3.line()
+  .x(dataPoint => xScale(dataPoint.Date))
+  .y(dataPoint => yScale(dataPoint.Confirmed));
+  
+// Add path
+d3.select("svg")
+  .append("path")
+  .attr("transform", `translate(${margin.left},0)`)
+  .datum(data)
+  .attr("fill", "none")
+  .attr("stroke", "steelblue")
+  .attr("stroke-linejoin", "round")
+  .attr("stroke-linecap", "round")
+  .attr("stroke-width", 1.5)
+  .attr("d", line);
 
 // Add X axis
 d3.select("svg").append("g")
  .attr("transform",`translate(${margin.left},${margin.top + chart_height})`)
  .call(d3.axisBottom(x));
-}
+
+// Add Y axis
+d3.select("svg").append("g")
+ .attr("transform",`translate(${margin.left},${margin.top})`)
+ .call(d3.axisLeft(y));
