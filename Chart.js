c9c917f8769d2d86ab325415b54b7f04eba5bedd
chendg55 <110141958+chendg55@users.noreply.github.com>
2022-07-30 01:00:06 -0400

    Update Chart.js

--- Chart.js
+++ Chart.js
@@ -303,120 +303,144 @@
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("January <br> Cases: 6,120,765 <br> Deaths: 95,987 <br> Total Cases: 6,120,765 <br> Total Deaths: 95,987");
         d3.select("#Jan")
             .on("mouseover", function(){return tooltip_1.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_1.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_1.style("visibility", "hidden");});
             
         var tooltip_2 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("February <br> Cases: 2,398,659 <br> Deaths: 64,250 <br> Total Cases: 8,519,424 <br> Total Deaths: 160,237");
         d3.select("#Feb")
             .on("mouseover", function(){return tooltip_2.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_2.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_2.style("visibility", "hidden");});
             
         var tooltip_3 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("March <br> Cases: 1,803,940 <br> Deaths: 36,686 <br> Total Cases: 10,323,364 <br> Total Deaths: 196,923");
         d3.select("#Mar")
             .on("mouseover", function(){return tooltip_3.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_3.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_3.style("visibility", "hidden");});
             
         var tooltip_4 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("April <br> Cases: 1,860,904 <br> Deaths: 23,726 <br> Total Cases: 12,184,268 <br> Total Deaths: 220,649");
         d3.select("#Apr")
             .on("mouseover", function(){return tooltip_4.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_4.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_4.style("visibility", "hidden");});
             
         var tooltip_5 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("May <br> Cases: 915,772 <br> Deaths: 18,133 <br> Total Cases: 13,100,040 <br> Total Deaths: 238,782");
         d3.select("#May")
             .on("mouseover", function(){return tooltip_5.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_5.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_5.style("visibility", "hidden");});
             
         var tooltip_6 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("June <br> Cases: 397,959 <br> Deaths: 10,513 <br> Total Cases: 13,497,999 <br> Total Deaths: 249,295");
         d3.select("#Jun")
             .on("mouseover", function(){return tooltip_6.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_6.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_6.style("visibility", "hidden");});
             
         var tooltip_7 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("July <br> Cases: 1,315,293 <br> Deaths: 8,799 <br> Total Cases: 14,813,292 <br> Total Deaths: 258,094");
         d3.select("#Jul")
             .on("mouseover", function(){return tooltip_7.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_7.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_7.style("visibility", "hidden");});
             
         var tooltip_8 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("August <br> Cases: 4,250,390 <br> Deaths: 28,576 <br> Total Cases: 19,063,682 <br> Total Deaths: 286,670");
         d3.select("#Aug")
             .on("mouseover", function(){return tooltip_8.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_8.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_8.style("visibility", "hidden");});
             
         var tooltip_9 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("September <br> Cases: 4,121,812 <br> Deaths: 59,977 <br> Total Cases: 23,185,494 <br> Total Deaths: 346,647");
         d3.select("#Sep")
             .on("mouseover", function(){return tooltip_9.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_9.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_9.style("visibility", "hidden");});
             
         var tooltip_10 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("October <br> Cases: 2,498,528 <br> Deaths: 48,017 <br> Total Cases: 25,684,022 <br> Total Deaths: 394,664");
         d3.select("#Oct")
             .on("mouseover", function(){return tooltip_10.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_10.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_10.style("visibility", "hidden");});
             
         var tooltip_11 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("November <br> Cases: 2,540,514 <br> Deaths: 34,264 <br> Total Cases: 28,224,536 <br> Total Deaths: 428,928");
         d3.select("#Nov")
             .on("mouseover", function(){return tooltip_11.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_11.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_11.style("visibility", "hidden");});
             
         var tooltip_12 = d3.select("#my_dataviz")
             .append("div")
             .style("position", "absolute")
             .style("visibility", "hidden")
+            .style("font-size", "34px")
+            .style("left", width / 4)
+            .style("top", height / 4)
             .html("December <br> Cases: 6,135,065 <br> Deaths: 43,509 <br> Total Cases: 34,359,601 <br> Total Deaths: 472,437");
         d3.select("#Dec")
             .on("mouseover", function(){return tooltip_12.style("visibility", "visible");})
-            .on("mousemove", function(){return tooltip_12.style("top", (event.pageY)+"px").style("left",(event.pageX+20)+"px");})
             .on("mouseout", function(){return tooltip_12.style("visibility", "hidden");});
             
         // Add line for cases
